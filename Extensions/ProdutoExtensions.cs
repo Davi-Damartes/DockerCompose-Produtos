3cea4349d80d0cbc2c54d62f11f7304135be89d9
Davi Lima <davidamartesgit@Gmail.com>
2024-07-26 19:55:27 -0300

    Código para conexão docker

--- Extensions/ProdutoExtensions.cs
+++ Extensions/ProdutoExtensions.cs
@@ -9,7 +9,8 @@ namespace DockerProdutos.Extensions
         {
             services.AddDbContext<ProdutoContext>(options =>
             {
-                options.UseSqlServer("DATA source=db");
+                options.UseSqlServer("");
+                //docker run -e "ACCEPT_EULA=Y" -e "MSSQL_SA_PASSWORD=senhaProduto01" -e "MSSQL_PID=Express" -p 1200:1200 --name SqlServer-Docker -d mcr.microsoft.com/mssql/server:2019-latest 
 
             });
         }
